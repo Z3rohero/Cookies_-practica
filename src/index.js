b9bea3fe04e9bdd51c02ce5cfab681753f1f65ad
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2022-10-24 21:26:28 +0000

    mas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.get('/setcookie', (req, res) => {
+  res.cookie('micokiie', 'xD',{
+    httpOnly:true,
+    secure:true,
+    sameSite:'strict'
+    //maxAge: 7000,
+    //expires: new Date("2022-12-31"),
+      
+  })
   res.send('Hello Express app!')
 });
 
