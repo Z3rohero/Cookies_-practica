27f84d250fee9818c88551ebf428d3325f4c7fbf
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2022-10-24 21:34:40 +0000

    listo

--- src/index.js
+++ src/index.js
@@ -10,17 +10,30 @@ app.get('/', (req, res) => {
 });
 
 app.get('/setcookie', (req, res) => {
-  res.cookie('micokiie', 'xD',{
-    httpOnly:true,
-    secure:true,
-    sameSite:'strict'
+  res.cookie('micokiie', 'xD', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'lax'
     //maxAge: 7000,
     //expires: new Date("2022-12-31"),
-      
+
   })
   res.send('Hello Express app!')
+});
+
+app.get('/getcookie', (req, res) => {
+  console.log(req.cookies)
+  
+  res.send('leyendo cookies')
 });
 
 app.listen(3000, () => {
   console.log('server started');
 });
+
+app.get('/deletecookie', (req, res) => {
+  console.log(req.cookies)
+  res.clearCookie("micokiie")
+  res.send('leyendo cookies')
+});
+
