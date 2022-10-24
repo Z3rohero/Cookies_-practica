a7b4b17f9c06dd7e46ccaf2235d6a5f6055f2950
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2022-10-24 20:34:14 +0000

    montado el servidor

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
